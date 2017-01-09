0b2f7775190a31f9c09c9453a5f2938ff7be8c6d
kmclarnon <kmclarnon@hubspot.com>
2017-01-08 23:50:12 -0500

    add some comments

--- src/main/java/com/github/kmclarnon/interview/exercises/Main.java
+++ src/main/java/com/github/kmclarnon/interview/exercises/Main.java
@@ -6,7 +6,9 @@ import com.github.kmclarnon.interview.exercises.tests.Exercise1Tests;
 public class Main {
   public static void main(String[] args) {
     // uncomment these lines to run the exercises by themselves
+    // new Exercise1Tests().dryRun(new Exercise1());
 
+    // uncomment these lines to run the actual tests
     new Exercise1Tests().test(new Exercise1());
   }
 }
